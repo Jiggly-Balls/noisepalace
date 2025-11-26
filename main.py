d25ec26ab45c217549fd7e85cdb637e94beae908
Jiggly Balls <jigglyballs9000@gmail.com>
2025-11-27 00:56:25 +0530

    Added numpy test code

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-def main():
-    print("Hello from noisepalace!")
+import numpy
+
+
+def main() -> None:
+    num = numpy.random.default_rng(10)
+    # print(numpy.random.seed)
+    # print(num.integers(0, 129))
+    # print(num.integers(0, 129))
+    # print(num.integers(0, 129))
+    # print(num.integers(0, 129))
+    # print()
+    # print(num.integers(0, 256, size=256))
+
+    print(num.random(10))
+    print(num.random())
+    print(num.random())
+    print()
+    print(numpy.sin(90 * numpy.pi / 180))
 
 
 if __name__ == "__main__":
